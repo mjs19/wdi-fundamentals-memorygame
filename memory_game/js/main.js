a19863fc87f5fd610011514564f3d79663749104
Marita Sailor <marita.sailor@capitalone.com>
2017-07-19 14:00:32 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,22 @@
-// console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
-console.log("User flipped: " + cardOne);
-console.log("User flipped: " + cardTwo);
+console.log("User flipped " + cardsInPlay[0]);
+// console.log("User flipped " + cardTwo);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    // match
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
+}
