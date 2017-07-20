1decda49f48bbd0d86f39ce93fd6033827aed77f
Marita Sailor <marita.sailor@capitalone.com>
2017-07-20 11:00:55 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,44 @@
-var cards = ["queen", "queen", "king", "king"];
+// this is a comment in JS
+
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts_360.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds_360.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts_360.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds_360.png"
+  }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+alert("You found a match!");
 } else {
-console.log("Sorry, try again.");
+alert("Sorry, try again.");
 }
 }
 
 var flipCard = function(cardId){
   // card Id will be an index from the cards array
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
   if(cardsInPlay.length === 2){
     return checkForMatch();
