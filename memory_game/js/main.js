e10a37f5e90b2dd654b771e1d3fa520aa363997d
Marita Sailor <marita.sailor@capitalone.com>
2017-07-21 12:07:59 -0400

    playing with removeEventListener

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,38 +22,51 @@ var cards = [
   }
 ];
 var cardsInPlay = [];
-var clickAgain = false;
 var checkForMatch = function(){
 
+if(cardsInPlay.length === 2){
   if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
-    if(cardsInPlay[0].suit === cardsInPlay[1].suit){
-      // if same card was clicked twice it does NOT count as a match.
-      // after user gets alerted, I want them to be able to click another
-      // card and get the correct notification.
-      clickAgain = true;
-      cardsInPlay.pop(); // remove the duplicate
-      alert("Oops, you clicked the same card twice.");
-
+    alert("You found a match! Click New Game to reset.");
+    // remove event listener from other cards
+    var dom = document.getElementsByTagName('img');
+    console.log(dom);
+    for(i = 0; i < dom.length; i++){
+      var cardRank = cards[i].rank;
+      var cardSuit = cards[i].suit;
+      console.log("card rank: " + cardRank + " suit: " + cardSuit);
+      for(i = 0; i < cardsInPlay.length; i++){ // card is not in play
+        var cardRank2 = cardsInPlay[i].rank;
+        var cardSuit2 = cardsInPlay[i].rank;
+        if(cardRank !== cardRank2 && cardSuit !== cardSuit2){
+          console.log("card not in play (rank): " + cardRank2.rank) +
+          "card not in play (suit): " + cardRank2.suit;
+          var rm = cards[i];
+          // remove event listener if card is not in play
+          rm.removeEventListener('click', flipCard);
+          console.log(rm);
+        }
+      }
     }
-    else{
-      alert("You found a match!")
-    }
-// no match found
-} else {
-alert("Sorry, try again.");
+} else { //no match found
+  alert("Sorry, try again.");
+  location.reload();
 }
 }
-
+// cardsInPlay.length >= 3
+else if (cardsInPlay.length >= 3){
+  alert("Refresh page");
+}
+}
 
 var flipCard = function(){
+  this.removeEventListener('click', flipCard);
   var cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId]);
   this.setAttribute("src", cards[cardId].cardImage);
 
-  if(cardsInPlay.length === 2 && !clickAgain){
+  if(cardsInPlay.length === 2){
     return setTimeout(checkForMatch, 200);
   } else{
-    clickAgain = false;
     return setTimeout(checkForMatch, 200);
   }
 }
@@ -66,6 +79,10 @@ var createBoard = function(){
     cardElement.addEventListener('click', flipCard);
     document.getElementById('game-board').appendChild(cardElement);
   }
+}
+
+var reload = function(){
+
 }
 
 createBoard();
